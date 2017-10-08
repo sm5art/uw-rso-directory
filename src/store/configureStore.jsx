6d960df54af99bba68b5bd25cf9732a35d9d020f
Artur K <art@network.ru>
2017-10-08 14:28:24 -0700

    finally

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleWare } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
@@ -6,7 +6,7 @@ export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleWare(thunk)
+    applyMiddleware(thunk)
   );
 
   return store;
--- static/bundle.js
+++ static/bundle.js
@@ -68492,7 +68492,7 @@ var _reducers2 = _interopRequireDefault(_reducers);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function configureStore(initialState) {
-  var store = (0, _redux.createStore)(_reducers2.default, initialState, (0, _redux.applyMiddleWare)(_reduxThunk2.default));
+  var store = (0, _redux.createStore)(_reducers2.default, initialState, (0, _redux.applyMiddleware)(_reduxThunk2.default));
 
   return store;
 }
